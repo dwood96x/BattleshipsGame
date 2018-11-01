5c87515ccae9ccb9ec3f9b95985f8961c3d0550f
David Wood <dwood96x@gmail.com>
2018-11-01 10:15:21 -0400

    Added in scaling to board creation.

--- Battleships/Assets/GameBoard.cs
+++ Battleships/Assets/GameBoard.cs
@@ -5,11 +5,14 @@ using System.Collections;
 public class GameBoard : MonoBehaviour
 {
     public GameObject mCoordPrefab;
+    //private Resolution GetResolution = Screen.currentResolution;
 
     [HideInInspector]
-    public Coord[,] mAllCoords = new Coord[10, 10];
+    public Coord[,] mPOneCoords = new Coord[10, 10];
+    public Coord[,] mPTwoCoords = new Coord[10, 10];
 
-    public void CreateBoard()
+    // Creates top board
+    public void CreateBoardOne(int Heightmod)
     {
         for (int x = 0; x < 10; x++)
         {
@@ -20,25 +23,38 @@ public class GameBoard : MonoBehaviour
 
                 // Position
                 RectTransform rectTransform = newCoord.GetComponent<RectTransform>();
-                rectTransform.anchoredPosition = new Vector2((x * 50) + 25, (y * 50) + 25);
+                rectTransform.anchoredPosition = new Vector2((x * 50 * Heightmod) + (25 * Heightmod), (y * 50 * Heightmod) + (25 * Heightmod));
 
                 // Setup
-                mAllCoords[x, y] = newCoord.GetComponent<Coord>();
-                mAllCoords[x, y].Setup(new Vector2Int(x, y), this);
+                mPOneCoords[x, y] = newCoord.GetComponent<Coord>();
+                mPOneCoords[x, y].Setup(new Vector2Int(x, y), this);
             }
         }
-        // Colors the coords
-        /*
-        for (int x = 0; x < 10; x += 2)
+    }
+    // Creates top board for the enemy
+    public void CreateBoardTwo(int Heigthmod)
+    {
+        for (int x = 0; x < 10; x++)
         {
             for (int y = 0; y < 10; y++)
             {
-                int offset = (y % 2 != 0) ? 0 : 1;
-                int finalX = x + offset;
+                // Creates Coord
+                GameObject newCoord = Instantiate(mCoordPrefab, transform);
 
-                mAllCoords[finalX, y].GetComponent<Image>().color = new Color32(090, 210, 097, 255);
+                // Position
+                RectTransform rectTransform = newCoord.GetComponent<RectTransform>();
+                rectTransform.anchoredPosition = new Vector2((x * 50 * Heigthmod) + (25 * Heigthmod), (y * 50 * Heigthmod) + (550 * Heigthmod));
+
+                // Setup
+                mPTwoCoords[x, y] = newCoord.GetComponent<Coord>();
+                mPTwoCoords[x, y].Setup(new Vector2Int(x, y), this);
             }
         }
-        */
+    }
+    // Checks the height modifier for scaling board purposes. Assumes height of 1080 in canvas scaler
+    public int GetHeightMod(Resolution GetResolution)
+    {
+        int mod = (GetResolution.height / 1080 );
+        return mod;
     }
 }
--- Battleships/Assets/GameManager.cs
+++ Battleships/Assets/GameManager.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class GameManager : MonoBehaviour
 {
     public GameBoard mBoard;
+    private Resolution GetResolution;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-        mBoard.CreateBoard();
+        GetResolution = Screen.currentResolution;
+        mBoard.CreateBoardOne(mBoard.GetHeightMod(GetResolution));       
+        mBoard.CreateBoardTwo(mBoard.GetHeightMod(GetResolution));
 	}
 }
