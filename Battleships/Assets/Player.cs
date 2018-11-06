47b10bc10a7cf201042d1166e0efd0c2fba8f72f
David Wood <dwood96x@gmail.com>
2018-11-06 09:44:06 -0500

    Added player class. Updated framework version.

---
+++ Battleships/Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private string Username;
+    private int PlayerID;
+    private int PlayerNum;
+    //Player num might be unessissary as both players will be instantiated as objects
+    private bool HasShips;
+    private bool MyTurn;
+
+    public void Ready()
+    {
+        throw new System.NotImplementedException();
+    }
+    public void StartTurn()
+    {
+        throw new System.NotImplementedException();
+    }
+    public void EndTurn()
+    {
+        throw new System.NotImplementedException();
+    }
+}
